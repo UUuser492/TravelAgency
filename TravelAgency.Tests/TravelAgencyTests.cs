e571513233a19afe5f0f4ef5dffc723f872378f1
UUuser492 <codexcepper@gmail.com>
2022-11-14 18:35:15 +0200

    Failed GetCountrues method

--- TravelAgency.Tests/TravelAgencyTests.cs
+++ TravelAgency.Tests/TravelAgencyTests.cs
@@ -1,10 +1,25 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace TravelAgency.Tests
 {
     public class TravelAgencyTests
     {
-        
+        [Fact]
+        public void Return_List_Of_Countries()
+        {
+            //Arrange
+            var Agency = new TravelAgency();
+            Agency.AddCountry("Germany");
+            //Act
+
+            List<string> expected = new List<string>
+            {
+            "Germany",
+            };
+            //Assert
+            Assert.Equal(expected, Agency.GetCountries());
+        }
     }
 }
---
+++ TravelAgency/TravelAgency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelAgency
+{
+    public class TravelAgency
+    {
+        public List<string> Countries = new List<string>();
+
+        public void AddCountry(string country)
+        {
+
+        }
+
+        public List<string> GetCountries()
+        {
+            return Countries;
+        }
+    }
+}
